931cfec60a39a4f0e917dfaa63dd80cdc29532e7
tvinay97 <tvinay97@gmail.com>
2017-01-17 12:03:15 +0530

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+var getConfig = require('hjs-webpack');
+
+
+module.exports = getConfig({
+  in: 'src/app.js',
+  out: 'dist',
+  clearBeforeBuild: true,
+  html: function (data) {
+    return {
+      'index.html': [
+        '<!DOCTYPE html>',
+        '<html lang="en">',
+          '<head>',
+            '<meta charset="UTF-8">',
+            '<meta name="viewport" content="width=device-width, initial-scale=1">',
+            '<link href="' + data.css + '" rel="stylesheet" type="text/css" />',
+            '<title>Weather app built with reactjs</title>',
+          '</head>',
+          '<body>',
+            '<script src="' + data.main + '"></script>',
+          '</body>',
+          
+        '</html>'
+      ].join('')
+    }
+  }
+});
